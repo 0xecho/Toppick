bd538a752b6d8f2dc97e0d022a5b4899e1bd2c69
0xecho <0xecho@proto-code.com>
2022-01-13 22:40:47 +0300

    Adds models

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    telegram_id = models.CharField(max_length=255, blank=True, null=True)
+    telegram_username = models.CharField(max_length=255, blank=True, null=True)
+    is_public = models.BooleanField(default=True)
 
     def __str__(self):
         return self.email
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import CustomUser
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.IntegerField()
+    poster = models.ImageField(upload_to='posters/')
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Director(models.Model):
+    name = models.CharField(max_length=255)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
+
+class MovieSeen(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.movie.title
+
+class MovieComparision(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    better_movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='better_movie')
+    worse_movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='worse_movie')
+
+    def __str__(self):
+        return f'{self.better_movie.title} > {self.worse_movie.title}'
