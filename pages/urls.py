4eb2697bdb296b21e15ecdd224ccd97b654bd92f
0xecho <0xecho@proto-code.com>
2022-01-17 21:50:59 +0300

    Adds broadcast feature

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, BroadcastPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('broadcast/', BroadcastPageView.as_view(), name='broadcast'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.conf import settings
+import botogram
 
+from accounts.models import CustomUser
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
@@ -7,3 +13,22 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+class BroadcastPageView(LoginRequiredMixin, TemplateView):
+    template_name = 'pages/broadcast.html'
+    
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('home'))
+        return super().dispatch(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        # take message from post request and send it to all users
+        message = request.POST.get('message')
+        bot = botogram.create(settings.TELEGRAM_BOT_TOKEN)
+        for user in CustomUser.objects.all():
+            try:
+                bot.chat(user.telegram_id).send(message)
+            except:
+                pass
+        return redirect(reverse_lazy('broadcast'))
